d9f6d771a1023f6eda1d764bd68fbd392c1b909f
Maxence.F <maxence.fourrier@gmail.com>
2019-09-03 23:24:10 -0400

    Dir & test

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView>
+         <View>
+              <Text>SafeAreaView - Implementation</Text>
+         </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,6 +21,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#ddd'
+  }
 });
