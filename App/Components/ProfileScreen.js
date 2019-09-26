d4531962a8b38b11394f9e9db170c50d2c1147d0
Maxence.F <maxence.fourrier@gmail.com>
2019-09-25 21:42:42 -0400

    Navigation Screens

---
+++ App/Components/ProfileScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Text, View } from "react-native";
+
+class ProfileScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Profile Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default ProfileScreen;
